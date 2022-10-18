438f0e09ae167efad2a1a164e06c829d8ece20d9
Nic0lasDev <94779829+Nic0lasDev@users.noreply.github.com>
2022-10-18 11:36:08 +0200

    lightbox ok, modale created

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-// i create a function that changes the number
-// of articles and changes it into DOM
 let total = 0;
 const countEl = document.querySelector('#counter');
 const plusEl = document.querySelector('#add');
 const minusEl = document.querySelector('#remove');
 const cartEl = document.querySelector('.navbar__right-count');
+const cartIcon = document.querySelector('.navbar__right-cart');
 const addEl = document.querySelector('.add');
+const largeImage = document.querySelector('.main__left-large');
+const lightbox = document.querySelector('.ligth-box');
+const close = document.querySelector('.close');
+const modale = document.querySelector('.modale');
 
+// add product into the modale when clicking on Add to cart
+
+//i open the modale on click on "cart"
+
+function openModale() {
+  cartIcon.addEventListener('click', () => {
+    modale.classList.toggle('isActive');
+    console.log('hi');
+  });
+}
+//i open light-box on click in the large image
+function openLightbox() {
+  largeImage.addEventListener('click', () => {
+    lightbox.classList.add('isActive');
+  });
+  close.addEventListener('click', () => {
+    lightbox.classList.remove('isActive');
+  });
+}
+
+// i create a function that changes the number
+// of articles and changes it into DOM
 function changeArticleCount() {
   plusEl.addEventListener('click', () => {
     total++;
@@ -50,7 +75,7 @@ function setSelected() {
     });
   });
   const images = document.querySelectorAll('.image');
-  const largeImage = document.querySelector('.main__left-large');
+  const largeImage2 = document.querySelector('.large');
   images.forEach((img) => {
     img.addEventListener('click', () => {
       let source = img.src;
@@ -58,21 +83,25 @@ function setSelected() {
         source == 'http://127.0.0.1:5500/images/image-product-3-thumbnail.jpg'
       ) {
         largeImage.src = 'http://127.0.0.1:5500/images/image-product-3.jpg';
+        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-3.jpg';
       }
       if (
         source == 'http://127.0.0.1:5500/images/image-product-1-thumbnail.jpg'
       ) {
         largeImage.src = 'http://127.0.0.1:5500/images/image-product-1.jpg';
+        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-1.jpg';
       }
       if (
         source == 'http://127.0.0.1:5500/images/image-product-2-thumbnail.jpg'
       ) {
         largeImage.src = 'http://127.0.0.1:5500/images/image-product-2.jpg';
+        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-2.jpg';
       }
       if (
         source == 'http://127.0.0.1:5500/images/image-product-4-thumbnail.jpg'
       ) {
         largeImage.src = 'http://127.0.0.1:5500/images/image-product-4.jpg';
+        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-4.jpg';
       }
     });
   });
@@ -81,3 +110,5 @@ function setSelected() {
 setSelected();
 changeArticleCount();
 setActived();
+openLightbox();
+openModale();
