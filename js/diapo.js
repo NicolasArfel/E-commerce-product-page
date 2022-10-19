a367aa2ee61b650962bacc97869bc555c26e70fd
Nic0lasDev <94779829+Nic0lasDev@users.noreply.github.com>
2022-10-19 15:35:27 +0200

    challenge complete

--- js/diapo.js
+++ js/diapo.js
@@ -10,13 +10,37 @@ const smallImageArray = [
   'http://127.0.0.1:5500/images/image-product-3-thumbnail.jpg',
   'http://127.0.0.1:5500/images/image-product-4-thumbnail.jpg',
 ];
+let index = 0;
+const images = document.querySelectorAll('.image');
+const image = document.querySelector(`.image${index}`);
+const largeImage1 = document.querySelector('.main__left-large');
+const largeImage2 = document.querySelector('.large');
 
 function changeImage() {
-  const leftArrow = document.querySelector('.left');
-  const rightArrow = document.querySelector('.right');
+  const leftArrow = document.querySelectorAll('.left');
+  const rightArrow = document.querySelectorAll('.right');
 
-  leftArrow.addEventListener('click', () => {
-    console.log('hi');
+  leftArrow.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+      index--;
+      if (index < 0) {
+        index = 3;
+      }
+
+      largeImage1.src = largeImageArray[index];
+      largeImage2.src = largeImageArray[index];
+    });
+  });
+
+  rightArrow.forEach((arrow) => {
+    arrow.addEventListener('click', () => {
+      index++;
+      if (index > 3) {
+        index = 0;
+      }
+      largeImage1.src = largeImageArray[index];
+      largeImage2.src = largeImageArray[index];
+    });
   });
 }
 changeImage();
@@ -30,37 +54,34 @@ function setSelected() {
       addClass(img, 'selected');
     });
   });
-  const images = document.querySelectorAll('.image');
-  const largeImage2 = document.querySelector('.large');
-  images.forEach((img) => {
-    img.addEventListener('click', () => {
-      let source = img.src;
-      if (
-        source == 'http://127.0.0.1:5500/images/image-product-3-thumbnail.jpg'
-      ) {
-        largeImage.src = 'http://127.0.0.1:5500/images/image-product-3.jpg';
-        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-3.jpg';
-      }
-      if (
-        source == 'http://127.0.0.1:5500/images/image-product-1-thumbnail.jpg'
-      ) {
-        largeImage.src = 'http://127.0.0.1:5500/images/image-product-1.jpg';
-        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-1.jpg';
-      }
-      if (
-        source == 'http://127.0.0.1:5500/images/image-product-2-thumbnail.jpg'
-      ) {
-        largeImage.src = 'http://127.0.0.1:5500/images/image-product-2.jpg';
-        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-2.jpg';
-      }
-      if (
-        source == 'http://127.0.0.1:5500/images/image-product-4-thumbnail.jpg'
-      ) {
-        largeImage.src = 'http://127.0.0.1:5500/images/image-product-4.jpg';
-        largeImage2.src = 'http://127.0.0.1:5500/images/image-product-4.jpg';
-      }
-    });
-  });
 }
+images.forEach((img) => {
+  img.addEventListener('click', () => {
+    let source = img.src;
+
+    switch (source) {
+      case smallImageArray[0]:
+        largeImage1.src = largeImageArray[0];
+        largeImage2.src = largeImageArray[0];
+        break;
+      case smallImageArray[1]:
+        largeImage1.src = largeImageArray[1];
+        largeImage2.src = largeImageArray[1];
+        break;
+      case smallImageArray[2]:
+        largeImage1.src = largeImageArray[2];
+        largeImage2.src = largeImageArray[2];
+        break;
+      case smallImageArray[3]:
+        largeImage1.src = largeImageArray[3];
+        largeImage2.src = largeImageArray[3];
+        break;
+
+      default:
+        largeImage1.src = largeImageArray[0];
+        largeImage2.src = largeImageArray[0];
+    }
+  });
+});
 
 setSelected();
